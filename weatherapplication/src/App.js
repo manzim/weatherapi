dd66e56c98b9c524b3acfd1ca0e39f63cadc5c97
manzim <manzim.ruet@gmail.com>
2021-03-09 00:36:30 +0600

    project UI started

--- weatherapplication/src/App.js
+++ weatherapplication/src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
- 
+
+import './index.css'
+
+const api = {
+  key: "877f657465bd8ca9f9d68fc9420eb65c",
+  base: "http://api.weatherstack.com/"
+}
+
 function App() {
   return (
-    <div className="App">
-      hellow
+    <div className="app">
+      <section>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          // onChange={e => setQuery(e.target.value)}
+          // value={query}
+          // onKeyPress={search}
+          />
+        </div>
+        <div className="">
+          <div className="location-box">
+            <div className="location">
+              <div className="date">
+                {new Date().toLocaleTimeString()}
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

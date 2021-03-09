7553ed61e782dd8badec8348ccd78076ddd7d41c
manzim <manzim.ruet@gmail.com>
2021-03-10 01:08:49 +0600

    UI set but api access key not found!

--- weatherapplication/src/App.js
+++ weatherapplication/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import moment from 'moment';
 
 import './index.css'
 
 const api = {
-  key: "877f657465bd8ca9f9d68fc9420eb65c",
-  base: "http://api.weatherstack.com/"
+  key: "03afe73b8207e542f76f20091e01d131",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
   return (
     <div className="app">
       <section>
@@ -16,17 +33,22 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..."
-          // onChange={e => setQuery(e.target.value)}
-          // value={query}
-          // onKeyPress={search}
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+            onKeyPress={search}
           />
         </div>
         <div className="">
           <div className="location-box">
             <div className="location">
-              <div className="date">
-                {new Date().toLocaleTimeString()}
-              </div>
+              New jersey
+             </div>
+            <div className="date">
+              {moment().format("dddd, MMMM Do YYYY, h:mm a")}
+            </div>
+            <div className="weather-box">
+              <div className="temp"> 15* C </div>
+              <div className="weather"> Sunny </div>
             </div>
 
           </div>
